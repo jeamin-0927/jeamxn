d65f2944f6a811d421e3fe8d936f89dd0c73a2ae
최재민 <admin@chicken-moo.com>
2023-06-20 20:54:22 +0900

    Default Setting

---
+++ src/components/DefaultHead.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from "react";
+
+const DefaultHead = ({ children }) => {
+  return (
+    <Head>
+      <title>JEAMXN :: 최재민 포트폴리오</title>
+      <meta name="description" content="JEAMXN :: 최재민 포트폴리오" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      {/* <script defer data-domain="dimigo.net" src="https://analytics.2w.vc/js/script.js"></script> */}
+      {children}
+    </Head>
+  );
+};
+
+export default DefaultHead;
---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+
+import styles from "%/Home.module.css";
+import DefaultHead from "@/components/DefaultHead";
+
+export default function Home() {
+  return (
+    <>
+      <DefaultHead />
+      <main className={styles.main}>
+        
+      </main>
+    </>
+  );
+}
