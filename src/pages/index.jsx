c1b3f4bcd661e04f10bf889b137973bbfb9c8e17
최재민 <admin@chicken-moo.com>
2023-06-21 08:51:53 +0900

    rm trash code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
-import { usePdf } from "@mikecousins/react-pdf";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 
 import styles from "%/Home.module.css";
 import DefaultHead from "@/components/DefaultHead";
