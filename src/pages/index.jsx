7bdaa1301c2775872e6d7f8d660cc550b08fb302
최재민 <admin@chicken-moo.com>
2023-06-21 08:50:44 +0900

    figma

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { usePdf } from "@mikecousins/react-pdf";
+import React, { useEffect, useRef, useState } from "react";
 
 import styles from "%/Home.module.css";
 import DefaultHead from "@/components/DefaultHead";
@@ -8,7 +9,12 @@ export default function Home() {
     <>
       <DefaultHead />
       <main className={styles.main}>
-        <div>test</div>
+        <iframe 
+          // style="border: 1px solid rgba(0, 0, 0, 0.1);" 
+          className={styles.iframe}
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FFT4RGqm6kyarSaJRzpUeUS%2FJeamin-Portfolio%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D1-3%26viewport%3D-5811%252C-125%252C0.59%26scaling%3Dscale-down-width%26starting-point-node-id%3D1%253A3" 
+          allowFullScreen 
+        />
       </main>
     </>
   );
