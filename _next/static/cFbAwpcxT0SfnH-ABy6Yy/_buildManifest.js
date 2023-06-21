1d8379d4b1b96eb45b07711aa5093b3d1490fe31
최재민 <admin@chicken-moo.com>
2023-06-21 16:24:57 +0900

    Updates

---
+++ _next/static/cFbAwpcxT0SfnH-ABy6Yy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5c1ee4a9edffc87a.css","static/chunks/pages/index-7fc12a9aa30de70c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
