1fb2bb4a83d496a49a36b3944254afc82c1d21be
최재민 <admin@chicken-moo.com>
2023-06-21 09:49:54 +0900

    og:image

--- src/components/DefaultHead.jsx
+++ src/components/DefaultHead.jsx
@@ -8,6 +8,7 @@ const DefaultHead = ({ children }) => {
       <meta name="description" content="JEAMXN :: 최재민 포트폴리오" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+      <meta property="og:image" content="https://jeamxn.dev/og_image.png" />
       {/* <script defer data-domain="dimigo.net" src="https://analytics.2w.vc/js/script.js"></script> */}
       {children}
     </Head>
