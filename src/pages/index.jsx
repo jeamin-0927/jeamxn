d57d13311659dfd874b8f715a3af894c428ebbf1
최재민 <admin@chicken-moo.com>
2023-06-21 09:37:44 +0900

    figma url change

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
         <iframe 
           // style="border: 1px solid rgba(0, 0, 0, 0.1);" 
           className={styles.iframe}
-          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FFT4RGqm6kyarSaJRzpUeUS%2FJeamin-Portfolio%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D1-3%26viewport%3D-5811%252C-125%252C0.59%26scaling%3Dscale-down-width%26starting-point-node-id%3D1%253A3" 
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FFT4RGqm6kyarSaJRzpUeUS%2FJeamin-Portfolio%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D1-3%26viewport%3D285%252C398%252C0.04%26scaling%3Dscale-down-width%26starting-point-node-id%3D1%253A3"
           allowFullScreen 
         />
       </main>
