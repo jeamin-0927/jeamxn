4ed1511d0a7bbd860beb2c01e4165993b3ec31db
최재민 <admin@chicken-moo.com>
2023-06-21 16:25:31 +0900

    rm trash code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import styles from "%/Home.module.css";
 import DefaultHead from "@/components/DefaultHead";
